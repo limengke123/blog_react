8347fa5ca44f8bbc5d7a6117a4760b234af94fab
limengke <752140235@qq.com>
2017-12-16 19:39:19 +0800

    导航

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,10 +2,12 @@
  * Created by li on 2017/12/14 17:12.
  */
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './index.less'
-import {Row, Col} from 'antd'
+import {Row, Col, Menu, Icon} from 'antd'
 import logo from '../../public/img/guapi_logo.png'
 import NavBtn from './navBtn'
+const {Item} = Menu
 export default class Header extends React.Component {
     constructor(){
         super(...arguments);
@@ -16,22 +18,45 @@ export default class Header extends React.Component {
             },{
                 title:"荒地",
                 route:"/"
-            }]
+            }],
+            current:"blog"
         }
+    }
+    handlerClick = (e) =>{
+        console.log('click',e)
+        this.setState({
+            current:e.key
+        })
     }
     render() {
         const {nav} = this.state;
         return (
             <Row className="com-header" type="flex">
                 <Col span={18} offset={3} className="nav">
-                    <div className="logo">
-                        <img  src={logo} alt="可爱的小瓜皮"/>
-                    </div>
-                    {
-                        nav.map((val,index)=>
-                            <NavBtn title={val.title} key={index} route={val.route}/>
-                        )
-                    }
+                    <Menu
+                        // onClick = {this.handlerClick}
+                        // selectedKeys = {[this.state.current]}
+                        mode="horizontal"
+                    >
+                            <div className="logo">
+                                <img  src={logo} alt="可爱的小瓜皮"/>
+                            </div>
+                        <Item key="mail">
+                            <Icon type="mail"/>
+                            <Link to="/blog">blog</Link>
+                        </Item>
+                        <Item key="app">
+                            <Icon type="appstore"/>app
+                        </Item>
+                        <Item type="fk">
+                            <Icon type="folder"/>文件
+                        </Item>
+                    </Menu>
+                    {/*{*/}
+                        {/*nav.map((val,index)=>*/}
+                            {/*<NavBtn title={val.title} key={index} route={val.route}/>*/}
+                        {/*)*/}
+                    {/*}*/}
                 </Col>
             </Row>
         )
